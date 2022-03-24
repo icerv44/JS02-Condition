9ac9cad1caeaa96f7a40d7db41e60fc08932d094
icerv44 <icerv44@hotmail.com>
2022-03-24 17:23:24 +0700

    JS02 HW Edit 2

--- 04-Logical-Operator/03/main.js
+++ 04-Logical-Operator/03/main.js
@@ -1,20 +1,15 @@
-let chk = 0;
+const input = prompt("Name : ");
+let user = "guest",
+  passWord;
 
-do {
-  const input = prompt("Name : ");
-  const passW = "";
-  let user, passWord;
-
-  input === "" || input * 1 === 0 ? (user = "guest") : "";
-  if (input === "codecamp") {
-    passW = prompt("Input Password");
-
-    if (passW === "123456") {
-      user = "codecamp";
-      alert("Wellcome");
-      chk = 1;
-    } else {
-      alert("Wrong Password");
-    }
+if (input === "" || input === null) {
+}
+if (input === "codecamp") {
+  const passW = prompt("Input Password");
+  if (passW === "123456") {
+    user = "codecamp";
+  } else {
+    alert("Wrong Password");
   }
-} while (chk == 0);
+}
+alert("Wellcome : " + user);
